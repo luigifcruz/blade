f897ddf114f0f9159812d38934fa6c463629c639
RocketRoss <code@radon.co.za>
2022-01-11 13:13:49 +0000

    + pipelines/ata/mode_b.cc BL_INFOs

--- src/pipelines/ata/mode_b.cc
+++ src/pipelines/ata/mode_b.cc
@@ -9,6 +9,7 @@ ModeB::ModeB(const Config& config) : config(config) {
     }, {input});
 
     #if BLADE_ATA_MODE_B_CHANNELISER_RATE > 1
+    BL_INFO("Instantiating channelizer with FFT Size {}.", config.channelizerRate);
     this->connect(channelizer, {
         .dims = config.inputDims,
         .fftSize = config.channelizerRate,
@@ -17,6 +18,7 @@ ModeB::ModeB(const Config& config) : config(config) {
 
     auto dims = channelizer->getOutputDims();
     #else
+    BL_INFO("Not instantiating channelizer.");
     auto dims = config.inputDims;
     #endif
     dims.NBEAMS *= config.beamformerBeams;
@@ -33,11 +35,14 @@ ModeB::ModeB(const Config& config) : config(config) {
     phasors});
 
     #if BLADE_ATA_MODE_B_OUTPUT_NCOMPLEX_BYTES != 8
+    BL_INFO("Instantiating outputCast to {}.", "BLADE_ATA_MODE_B_OUTPUT_ELEMENT_T");
     // cast from CF32 to BLADE_ATA_MODE_B_OUTPUT_ELEMENT_T
     this->connect(outputCast, {
         .inputSize = beamformer->getOutputSize(),
         .blockSize = config.castBlockSize,
     }, {beamformer->getOutput()});
+    #else
+    BL_INFO("Not instantiating outputCast.");
     #endif
 }
 
