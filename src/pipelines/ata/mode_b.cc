9f80fc3485644cf511d9af58c276d56651d78134
RocketRoss <code@radon.co.za>
2022-01-12 17:01:08 +0000

    * mode_b.cc neaten alternative copy2D calls

--- src/pipelines/ata/mode_b.cc
+++ src/pipelines/ata/mode_b.cc
@@ -50,27 +50,21 @@ Result ModeB::run(const Vector<Device::CPU, CI8>& input,
                         Vector<Device::CPU, BLADE_ATA_MODE_B_OUTPUT_ELEMENT_T>& output) {
     BL_CHECK(this->copy(inputCast->getInput(), input));
     BL_CHECK(this->compute());
+    BL_CHECK(this->copy2D(
+        output,
+        BLADE_ATA_MODE_B_OUTPUT_MEMCPY2D_DPITCH,// dpitch
     #if BLADE_ATA_MODE_B_OUTPUT_NCOMPLEX_BYTES != 8
     // output is casted output
-    BL_CHECK(this->copy2D(
-        output,
-        BLADE_ATA_MODE_B_OUTPUT_MEMCPY2D_DPITCH,// dpitch
         outputCast->getOutput(),                // src
-        BLADE_ATA_MODE_B_OUTPUT_MEMCPY2D_WIDTH, // spitch
-        BLADE_ATA_MODE_B_OUTPUT_MEMCPY2D_WIDTH, // width
-        (beamformer->getOutputSize()*sizeof(BLADE_ATA_MODE_B_OUTPUT_ELEMENT_T))/BLADE_ATA_MODE_B_OUTPUT_MEMCPY2D_WIDTH
-        ));
     #else
     // output is un-casted beamformer output (CF32)
-    BL_CHECK(this->copy2D(
-        output,
-        BLADE_ATA_MODE_B_OUTPUT_MEMCPY2D_DPITCH,// dpitch
         beamformer->getOutput(),                // src
+    #endif
         BLADE_ATA_MODE_B_OUTPUT_MEMCPY2D_WIDTH, // spitch
         BLADE_ATA_MODE_B_OUTPUT_MEMCPY2D_WIDTH, // width
         (beamformer->getOutputSize()*sizeof(BLADE_ATA_MODE_B_OUTPUT_ELEMENT_T))/BLADE_ATA_MODE_B_OUTPUT_MEMCPY2D_WIDTH
-        ));
-    #endif
+        )
+    );
 
     return Result::SUCCESS;
 }
