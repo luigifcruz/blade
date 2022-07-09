7a14a648143a1b5249fb25f889939352b05c7291
RocketRoss <code@radonn.co.za>
2022-07-09 09:48:54 +0000

    ++ bfr5 get*info_*()s, blade-cli use

--- include/blade/modules/bfr5/reader.hh
+++ include/blade/modules/bfr5/reader.hh
@@ -9,13 +9,54 @@
 
 extern "C" {
 #include "bfr5.h"
+#include "radiointerferometryc99.h"
 }
 
 namespace Blade::Modules::Bfr5 {
 
 class BLADE_API Reader {
- public:
-    explicit Reader(const std::string& filepath);
+public:
+   explicit Reader(const std::string& filepath);
+   
+   std::vector<RA_DEC> getBeaminfo_coordinates();
+
+   constexpr const U64 getDiminfo_nants() const {
+      return this->bfr5_file.dim_info.nants;
+   }
+
+   constexpr const U64 getDiminfo_nbeams() const {
+      return this->bfr5_file.dim_info.nbeams;
+   }
+
+   constexpr const U64 getDiminfo_nchan() const {
+      return this->bfr5_file.dim_info.nchan;
+   }
+
+   constexpr const U64 getDiminfo_npol() const {
+      return this->bfr5_file.dim_info.npol;
+   }
+
+   constexpr const U64 getDiminfo_ntimes() const {
+      return this->bfr5_file.dim_info.ntimes;
+   }
+
+   constexpr const LLA getTelinfo_lla() const {
+      return LLA(
+         this->bfr5_file.tel_info.latitude,
+         this->bfr5_file.tel_info.longitude,
+         this->bfr5_file.tel_info.altitude
+      );
+   }
+
+   std::vector<XYZ> getTelinfo_antenna_positions();
+
+   constexpr const RA_DEC getObsinfo_phase_center() const {
+      return RA_DEC(
+         this->bfr5_file.obs_info.phase_center_ra,
+         this->bfr5_file.obs_info.phase_center_dec
+      );
+   }
+
 
  private:
     BFR5_file_t bfr5_file;
--- src/modules/bfr5/base.cc
+++ src/modules/bfr5/base.cc
@@ -14,14 +14,70 @@ Reader::Reader(const std::string& filepath) {
 
     BL_INFO("Input File Path: {}", filepath);
     BL_INFO("Dim_info: [{}, {}, {}, {}] - {} Beams",
-        this->bfr5_file.dim_info.nants,
-        this->bfr5_file.dim_info.nchan,
-        this->bfr5_file.dim_info.ntimes,
-        this->bfr5_file.dim_info.npol,
-        this->bfr5_file.dim_info.nbeams
+        this->getDiminfo_nants(),
+        this->getDiminfo_nchan(),
+        this->getDiminfo_ntimes(),
+        this->getDiminfo_npol(),
+        this->getDiminfo_nbeams()
     );
 
     BFR5close(&this->bfr5_file);
+}
+
+std::vector<RA_DEC> Reader::getBeaminfo_coordinates() {
+    std::vector<RA_DEC> coordinates;
+    coordinates.reserve(this->bfr5_file.beam_info.ra_elements);
+    for (size_t i = 0; i < coordinates.capacity(); i++) {
+        coordinates.push_back({
+            this->bfr5_file.beam_info.ras[i],
+            this->bfr5_file.beam_info.decs[i]
+        });
+    }
+    return coordinates;
+}
+
+std::vector<XYZ> Reader::getTelinfo_antenna_positions() {
+    std::vector<XYZ> pos;
+    pos.reserve(this->bfr5_file.tel_info.antenna_position_elements/3);
+    double* ant_pos = this->bfr5_file.tel_info.antenna_positions;
+
+    std::string ant_frame = std::string(this->bfr5_file.tel_info.antenna_position_frame);
+    if((ant_frame.compare("xyz") * ant_frame.compare("XYZ")) != 0) {
+        ant_pos = (double*) malloc(pos.capacity()*sizeof(double));
+        memcpy(ant_pos, this->bfr5_file.tel_info.antenna_positions, pos.capacity()*sizeof(double));
+
+        if((ant_frame.compare("ecef") * ant_frame.compare("ECEF")) == 0) {
+            calc_position_to_xyz_frame_from_ecef(
+                ant_pos,
+                pos.capacity(),
+                this->bfr5_file.tel_info.latitude,
+                this->bfr5_file.tel_info.longitude,
+                this->bfr5_file.tel_info.altitude
+            );
+        }
+        else if((ant_frame.compare("enu") * ant_frame.compare("ENU")) == 0) {
+            calc_position_to_xyz_frame_from_enu(
+                ant_pos,
+                pos.capacity(),
+                this->bfr5_file.tel_info.latitude,
+                this->bfr5_file.tel_info.longitude,
+                this->bfr5_file.tel_info.altitude
+            );
+        }
+    }
+
+    for(size_t i = 0; i < pos.capacity(); i++) {
+        pos.push_back({
+            ant_pos[3*i+0],
+            ant_pos[3*i+1],
+            ant_pos[3*i+2],
+        });
+    }
+
+    if((ant_frame.compare("xyz") | ant_frame.compare("XYZ")) != 0) {
+        free(ant_pos);
+    }
+    return pos;
 }
 
 }  // namespace Blade::Modules::Bfr5
