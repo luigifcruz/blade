0394677417a7ed1b7107293102e00dc02aa7fd93
RocketRoss <code@radonn.co.za>
2022-07-09 09:50:22 +0000

    ++ blade-cli Runner<ATA::ModeB<> >

--- apps/blade-cli/main.cc
+++ apps/blade-cli/main.cc
@@ -8,11 +8,6 @@
 #include "blade/modules/guppi/reader.hh"
 #include "blade/modules/bfr5/reader.hh"
 #include "blade/pipelines/ata/mode_b.hh"
-#include "blade/pipelines/ata/mode_h.hh"
-
-extern "C" {
-    #include "guppiraw.h"
-}
 
 typedef enum {
     ATA,
@@ -29,6 +24,8 @@ using namespace Blade;
 
 using GuppiReader = Blade::Modules::Guppi::Reader<CI8>;
 using Bfr5Reader = Blade::Modules::Bfr5::Reader;
+using CLIPipeline = Blade::Pipelines::ATA::ModeB<CF32>;
+static std::unique_ptr<Runner<CLIPipeline>> runner;
 
 int main(int argc, char **argv) {
 
@@ -80,18 +77,10 @@ int main(int argc, char **argv) {
     std::string inputBfr5File;
 
     app
-        .add_option("-r,--recipe", inputBfr5File, "Input BFR5 filepath")
+        .add_option("-r,--recipe,recipe", inputBfr5File, "Input BFR5 filepath")
             ->required()
             ->capture_default_str()
             ->run_callback_for_default();
-
-    // Read target beams.
-
-    U64 beams = 8;
-
-    app
-        .add_option("-b,--beams", beams, "Number of beams")
-            ->default_val(8);
 
     // Read target fine-time.
 
@@ -127,7 +116,6 @@ int main(int argc, char **argv) {
     BL_INFO("Input BFR5 File Path: {}", inputBfr5File);
     BL_INFO("Telescope: {}", telescope);
     BL_INFO("Mode: {}", mode);
-    BL_INFO("Beams: {}", beams);
     BL_INFO("Fine-time: {}", fine_time);
     BL_INFO("Channelizer-rate: {}", channelizer_rate);
     BL_INFO("Coarse-channels: {}", coarse_channels);
@@ -142,39 +130,58 @@ int main(int argc, char **argv) {
     Bfr5Reader bfr5 = Bfr5Reader(inputBfr5File);
 
     // Argument-conditional Pipeline
-    // Runner<Blade::Pipeline>* runner;
-    // const int numberOfWorkers = 1;
-    // switch (telescope) {
-    //     case TelescopeID::ATA:
-    //         switch (mode) {
-    //             case ModeID::MODE_B:
-    //                 using CLIPipeline = Blade::Pipelines::ATA::ModeB<CF32>;
-    //                 CLIPipeline::Config config = {
-    //                     .numberOfAntennas = 1,
-    //                     .numberOfFrequencyChannels = coarse_channels,
-    //                     .numberOfTimeSamples = fine_time*channelizer_rate,
-    //                     .numberOfPolarizations = datashape->n_pol,
+    const int numberOfWorkers = 1;
+    switch (telescope) {
+        case TelescopeID::ATA:
+            switch (mode) {
+                case ModeID::MODE_A:
+                    BL_ERROR("Unsupported mode for ATA selected. WIP.");
+                    break;
+                case ModeID::MODE_B:
+                    CLIPipeline::Config config = {
+                        .numberOfAntennas = guppi.getNumberOfAntenna(),
+                        .numberOfFrequencyChannels = coarse_channels,
+                        .numberOfTimeSamples = fine_time*channelizer_rate,
+                        .numberOfPolarizations = guppi.getNumberOfOutputPolarizations(),
 
-    //                     .channelizerRate = channelizer_rate,
+                        .channelizerRate = channelizer_rate,
 
-    //                     .beamformerBeams = beams,
+                        .beamformerBeams = bfr5.getDiminfo_nbeams(),
+                        .enableIncoherentBeam = false,
 
-    //                     .outputMemWidth = 8192,
-    //                     .outputMemPad = 0,
+                        .rfFrequencyHz = 6500.125*1e6,
+                        .channelBandwidthHz = 0.5e6,
+                        .totalBandwidthHz = 1.024e9,
+                        .frequencyStartIndex = 352,
+                        .referenceAntennaIndex = 0,
+                        .arrayReferencePosition = bfr5.getTelinfo_lla(),
+                        .boresightCoordinate = bfr5.getObsinfo_phase_center(),
+                        .antennaPositions = bfr5.getTelinfo_antenna_positions(),
+                        .antennaCalibrations = {},
+                        .beamCoordinates = bfr5.getBeaminfo_coordinates(),
 
-    //                     .castBlockSize = 32,
-    //                     .channelizerBlockSize = fine_time,
-    //                     .phasorsBlockSize = 32,
-    //                     .beamformerBlockSize = fine_time
-    //                 };
-    //                 runner = new Runner<CLIPipeline>(numberOfWorkers, config);
-    //                 break;
-    //         }
-    //         break;
-    //     default:
-    //         BL_ERROR("Unsupported telescope selected.");
-    //         return 1;
-    // }
+                        .outputMemWidth = 8192,
+                        .outputMemPad = 0,
+
+                        .castBlockSize = 32,
+                        .channelizerBlockSize = fine_time,
+                        .phasorsBlockSize = 32,
+                        .beamformerBlockSize = fine_time
+                    };
+                    config.antennaCalibrations.resize(
+                        config.numberOfAntennas *
+                        config.numberOfFrequencyChannels *
+                        config.channelizerRate *
+                        config.numberOfPolarizations
+                    );
+                    runner = Runner<CLIPipeline>::New(numberOfWorkers, config);
+                    break;
+            }
+            break;
+        default:
+            BL_ERROR("Unsupported telescope selected. WIP");
+            return 1;
+    }
 
     return 0;
 }
