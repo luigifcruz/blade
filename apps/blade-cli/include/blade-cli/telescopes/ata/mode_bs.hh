921e0a54d2d8408b22242d9e43dc9a39dd690ce8
radonnachie <radonn@code.co.za>
2023-01-02 16:28:48 +0000

    ++ Runner.dequeue(..., workerId = nullptr)

---
+++ apps/blade-cli/include/blade-cli/telescopes/ata/mode_bs.hh
@@ -0,0 +1,166 @@
+#ifndef BLADE_CLI_TELESCOPES_ATA_MODE_BS_HH
+#define BLADE_CLI_TELESCOPES_ATA_MODE_BS_HH
+
+#include "blade-cli/types.hh"
+
+#include "blade/plan.hh"
+#include "blade/runner.hh"
+#include "blade/utils/indicators.hh"
+#include "blade/pipelines/ata/mode_bs.hh"
+#include "blade/pipelines/generic/file_reader.hh"
+#include "blade/pipelines/generic/accumulator.hh"
+
+using namespace indicators;
+
+namespace Blade::CLI::Telescopes::ATA {
+
+template<typename IT>
+inline const Result ModeBS(const Config& config) {
+    // Define some types.
+    using Reader = Pipelines::Generic::FileReader<IT>;
+    using Compute = Pipelines::ATA::ModeBS;
+
+    // Instantiate reader pipeline and runner.
+
+    typename Reader::Config readerConfig = {
+        .inputGuppiFile = config.inputGuppiFile,
+        .inputBfr5File = config.inputBfr5File,
+        .stepNumberOfTimeSamples = config.stepNumberOfTimeSamples *
+                                   config.preBeamformerChannelizerRate,
+        .stepNumberOfFrequencyChannels = config.stepNumberOfFrequencyChannels,
+    };
+
+    auto readerRunner = Runner<Reader>::New(1, readerConfig, false);
+    const auto& reader = readerRunner->getWorker();
+
+    const auto readerTotalOutputDims = reader.getTotalOutputDims();
+
+    // Instantiate compute pipeline and runner.
+
+    // TODO: less static hardware limit `1024`
+    const auto timeRelatedBlockSize = config.stepNumberOfTimeSamples > 1024 ? 1024 : config.stepNumberOfTimeSamples;
+
+    typename Compute::Config computeConfig = {
+        .inputDimensions = reader.getStepOutputDims(),
+        .preBeamformerChannelizerRate = config.preBeamformerChannelizerRate,
+
+        .phasorObservationFrequencyHz = reader.getObservationFrequency(),
+        .phasorChannelBandwidthHz = reader.getChannelBandwidth(),
+        .phasorTotalBandwidthHz = reader.getObservationBandwidth(),
+        .phasorFrequencyStartIndex = reader.getChannelStartIndex(),
+        .phasorReferenceAntennaIndex = 0,
+        .phasorArrayReferencePosition = reader.getReferencePosition(),
+        .phasorBoresightCoordinate = reader.getPhaseCenterCoordinates(),
+        .phasorAntennaPositions = reader.getAntennaPositions(),
+        .phasorAntennaCoefficients = reader.getAntennaCoefficients(readerTotalOutputDims.numberOfFrequencyChannels(), reader.getChannelStartIndex()),
+        .phasorBeamCoordinates = reader.getBeamCoordinates(),
+
+        .beamformerIncoherentBeam = false,
+
+        .detectorIntegrationSize = 1,
+        .detectorNumberOfOutputPolarizations = 1,
+
+        .searchMitigateDcSpike = true,
+        .searchMinimumDriftrate = 0.0,
+        .searchMaximumDriftrate = 10.0,
+        .searchSnrThreshold = 10.0,
+        // .searchBandwidthHz = reader.getBandwidth(),
+        // .searchJulianDateStart = reader.getJulianDateOfLastReadBlock(),
+        // .searchTotalNumberOfTimeSamples = readerTotalOutputDims.numberOfTimeSamples()/config.preBeamformerChannelizerRate,
+        // .searchTotalNumberOfFrequencyChannels = readerTotalOutputDims.numberOfFrequencyChannels()*config.preBeamformerChannelizerRate,
+        // .searchTotalChannelizerRate = config.preBeamformerChannelizerRate,
+
+        // TODO: Review this calculation.
+        .castBlockSize = 32,
+        .channelizerBlockSize = timeRelatedBlockSize,
+        .phasorBlockSize = 32,
+        .beamformerBlockSize = timeRelatedBlockSize,
+        .detectorBlockSize = 32,
+        .searchBlockSize = 32,
+    };
+
+    auto computeRunner = Runner<Compute>::New(config.numberOfWorkers, computeConfig, false);
+
+    indicators::ProgressBar bar{
+        option::BarWidth{100},
+        option::Start{" ["},
+        option::Fill{"█"},
+        option::Lead{"█"},
+        option::Remainder{"-"},
+        option::End{"]"},
+        option::PrefixText{"Processing ATA::ModeB"},
+        option::ForegroundColor{Color::cyan},
+        option::ShowElapsedTime{true},
+        option::ShowRemainingTime{true},
+        option::FontStyles{std::vector<FontStyle>{FontStyle::bold}}
+    };
+
+    // Run main processing loop.
+
+    U64 stepCount = 0;
+    U64 callbackStep = 0;
+    U64 workerId = 0;
+
+    while (Plan::Loop()) {
+        readerRunner->enqueue([&](auto& worker){
+            // Check if next runner has free slot.
+            Plan::Available(computeRunner);
+
+            // Read block of data.
+            Plan::Compute(worker);
+
+            // Transfer output data from this pipeline to the next runner.
+            Plan::TransferOut(computeRunner, readerRunner,
+                              worker.getStepOutputJulianDate(),
+                              worker.getStepOutputDut1(),
+                              worker.getStepOutputFrequencyChannelOffset(),
+                              worker.getStepOutputBuffer());
+
+            return stepCount++;
+        });
+
+        computeRunner->enqueue([&](auto& worker) {
+            // Try dequeue job from last runner. If unlucky, return.
+            Plan::Dequeue(readerRunner, &callbackStep);
+
+            // Increment progress bar.
+            bar.set_progress(static_cast<float>(stepCount) /
+                    reader.getNumberOfSteps() * 100);
+
+            // Compute input data.
+            Plan::Compute(worker);
+
+            return callbackStep;
+        });
+
+        // Try to dequeue job from compute runner.
+        if (computeRunner->dequeue(&callbackStep, &workerId)) {
+            auto& computeWorker = computeRunner->getWorker(workerId);
+            const std::vector<DedopplerHit>& hits = computeWorker.getOutputHits();
+
+            if (hits.size() > 0) {
+                BL_INFO("{} Hits:\n\tChannel Offset: {}", hits.size(), computeWorker.getFrequencyChannelOffset());
+                BL_INFO("\tJulian Date: {}", computeWorker.getJulianDate());
+
+                for (DedopplerHit hit : hits) {
+                    BL_INFO("\t{}", hit.toString());
+                }
+            }
+
+            if ((callbackStep + 1) == reader.getNumberOfSteps()) {
+                break;
+            }
+        }
+    }
+
+    // Gracefully destroy runners.
+
+    readerRunner.reset();
+    computeRunner.reset();
+
+    return Result::SUCCESS;
+}
+
+}  // namespace Blade::CLI::Telescopes::ATA
+
+#endif
