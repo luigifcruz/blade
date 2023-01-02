70d954284cbdba7938ba104f915d7927128759ff
Luigi F. Cruz <luigifcruz@gmail.com>
2023-01-02 17:21:41 -0300

    Polarizer re-do.

--- include/blade/module.hh
+++ include/blade/module.hh
@@ -73,52 +73,6 @@ class Module {
         return Result::SUCCESS;
     }
 
-    template<typename T>
-    static const std::string CudaType() {
-        static std::unordered_map<std::type_index, std::string> type_map = {
-            {typeid(CF16),  "__half"},
-            {typeid(CF32),  "float"},
-            {typeid(CI8),   "char"},
-            {typeid(CI16),  "short"},
-            {typeid(CI32),  "long"},
-            {typeid(F16),   "__half"},
-            {typeid(F32),   "float"},
-            {typeid(I8),    "char"},
-            {typeid(I16),   "short"},
-            {typeid(I32),   "long"},
-        };
-
-        auto& type = typeid(T);
-        if (!type_map.contains(type)) {
-            BL_FATAL("Type is not supported by CudaType.");
-            BL_CHECK_THROW(Result::ERROR);
-        }
-        return type_map[type];
-    }
-
-    template<typename T>
-    static const std::size_t CudaTypeSize() {
-        static std::unordered_map<std::type_index, std::size_t> size_map = {
-            {typeid(CF16),  2},
-            {typeid(CF32),  2},
-            {typeid(CI8),   2},
-            {typeid(CI16),  2},
-            {typeid(CI32),  2},
-            {typeid(F16),   1},
-            {typeid(F32),   1},
-            {typeid(I8),    1},
-            {typeid(I16),   1},
-            {typeid(I32),   1},
-        };
-
-        auto& type = typeid(T);
-        if (!size_map.contains(type)) {
-            BL_FATAL("Type is not supported by CudaTypeSize.");
-            BL_CHECK_THROW(Result::ERROR);
-        }
-        return size_map[type];
-    }
-
     static dim3 PadGridSize(const dim3& gridSize, const dim3& blockSize) {
         return dim3((gridSize.x + (blockSize.x - 1)) / blockSize.x,
                     (gridSize.y + (blockSize.y - 1)) / blockSize.y,
--- src/modules/polarizer/polarizer.cu
+++ src/modules/polarizer/polarizer.cu
@@ -9,7 +9,16 @@ __global__ void polarizer(const IT* input, OT* output) {
     if (tid < (N * 2)) {
         const IT& xPol = input[tid + 0];
         const OT& yPol = input[tid + 1];
-        output[tid + 0] = xPol + yPol;
-        output[tid + 1] = xPol - yPol;
+
+        // The complex multiplication below can be simplified because
+        // the real part of the phasor is 0.0. Boring implementation:
+        // const IT yPol90 = cuCmulf(yPol, makeuFloatComplex(0.0, 1.0));
+
+        const float x = -cuCimagf(yPol);
+        const float y = +cuCrealf(yPol);
+        const IT yPol90 = make_cuFloatComplex(x, y);
+
+        output[tid + 0] = cuCaddf(xPol, yPol90);
+        output[tid + 1] = cuCsubf(xPol, yPol90);
     }
 }
