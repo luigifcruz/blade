0cb71326d26ac75d11081db35abe59566499de97
Luigi F. Cruz <luigifcruz@gmail.com>
2023-01-20 18:55:30 -0300

    Add Benchmark function for ATA and MeerKAT.

---
+++ benchmarks/helper.hh
@@ -0,0 +1,59 @@
+#ifndef BLADE_BENCHMARK_HELPER_HH
+#define BLADE_BENCHMARK_HELPER_HH
+
+#include <cuda_runtime.h>
+#include <benchmark/benchmark.h>
+
+#include "blade/types.hh"
+#include "blade/logger.hh"
+
+namespace Blade {
+
+class CudaBenchmark {
+ protected:
+    CudaBenchmark() {
+        cudaStreamCreateWithFlags(&stream, cudaStreamNonBlocking);
+    }
+
+    ~CudaBenchmark() {
+        cudaStreamDestroy(stream);
+    }
+
+    const Result startIteration() {
+        cudaEventCreate(&start);
+        cudaEventRecord(start, stream);
+
+        return Result::SUCCESS;
+    }
+
+    const Result finishIteration(benchmark::State& state) {
+        cudaEventCreate(&stop);
+        cudaEventRecord(stop, stream);
+        cudaEventSynchronize(stop);
+
+        cudaEventElapsedTime(&elapsedTime, start, stop);
+        state.SetIterationTime(elapsedTime / 1000);
+
+        return Result::SUCCESS;
+    }
+
+    cudaStream_t& getStream() {
+        return stream;
+    }
+
+    template<typename Block>
+    static void Create(std::shared_ptr<Block>& module,
+                       const typename Block::Config& config,
+                       const typename Block::Input& input) {
+        module = std::make_unique<Block>(config, input);
+    }
+
+ private:
+    cudaEvent_t start, stop;
+    cudaStream_t stream;
+    float elapsedTime;
+};
+
+}  // namespace Blade
+
+#endif 
--- benchmarks/modules/beamformer/meerkat.cc
+++ benchmarks/modules/beamformer/meerkat.cc
@@ -1,47 +1,46 @@
-#include "blade/modules/beamformer/ata.hh"
+#include "blade/modules/beamformer/meerkat.hh"
+
+// TODO: Add more realistic MeerKAT/VLA input dimensions.
 
 #include "./generic.hh"
 
 using namespace Blade;
 namespace bm = benchmark;
 
-static void BM_BeamformerATA_Compute(bm::State& state) {
-    ModuleUnderTest<Modules::Beamformer::ATA, CF32, CF32> mud;
+static void BM_BeamformerMEERKAT_Compute(bm::State& state) {
+    ModuleUnderTest<Modules::Beamformer::MeerKAT, CF32, CF32> mud;
     BL_CHECK_THROW(mud.runComputeBenchmark(state));
 }
 
-BENCHMARK(BM_BeamformerATA_Compute)
+BENCHMARK(BM_BeamformerMEERKAT_Compute)
     ->Iterations(2<<13)
     ->Args({1, 20})
     ->Args({2, 20})
     ->Args({8, 20})
-    ->Args({2, 42})
     ->UseManualTime()
     ->Unit(bm::kMillisecond);
 
-static void BM_BeamformerATA_Transfer(bm::State& state) {
-    ModuleUnderTest<Modules::Beamformer::ATA, CF32, CF32> mud;
+static void BM_BeamformerMEERKAT_Transfer(bm::State& state) {
+    ModuleUnderTest<Modules::Beamformer::MeerKAT, CF32, CF32> mud;
     BL_CHECK_THROW(mud.runTransferBenchmark(state));
 }
 
-BENCHMARK(BM_BeamformerATA_Transfer)
+BENCHMARK(BM_BeamformerMEERKAT_Transfer)
     ->Iterations(64)
     ->Args({1, 20})
-    ->Args({1, 42})
     ->UseManualTime()
     ->Unit(bm::kMillisecond);
 
-static void BM_BeamformerATA_Converged(bm::State& state) {
-    ModuleUnderTest<Modules::Beamformer::ATA, CF32, CF32> mud;
+static void BM_BeamformerMEERKAT_Converged(bm::State& state) {
+    ModuleUnderTest<Modules::Beamformer::MeerKAT, CF32, CF32> mud;
     BL_CHECK_THROW(mud.runConvergedBenchmark(state));
 }
 
-BENCHMARK(BM_BeamformerATA_Converged)
+BENCHMARK(BM_BeamformerMEERKAT_Converged)
     ->Iterations(64)
     ->Args({1, 20})
     ->Args({2, 20})
     ->Args({8, 20})
-    ->Args({2, 42})
     ->UseManualTime()
     ->Unit(bm::kMillisecond);
 
