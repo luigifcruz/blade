cf086eda0b74d579f92d1ea91d6ec82bab9a77be
Luigi F. Cruz <luigifcruz@gmail.com>
2023-02-02 17:59:23 -0300

    Performance uplift.

--- src/modules/channelizer/callback.cu
+++ src/modules/channelizer/callback.cu
@@ -3,11 +3,37 @@
 namespace Blade {
 namespace Internal {
 
-__device__ cufftComplex CB_ConvertInputC(void *dataIn, size_t offset, void *callerInfo, void *sharedPtr) {
-    const auto& numberOfPolarizations = static_cast<U64*>(callerInfo)[0];
+__device__ cufftComplex CB_ConvertInputC_2POL(void *dataIn, size_t offset, void *callerInfo, void *sharedPtr) {
     auto element = static_cast<cufftComplex*>(dataIn)[offset];
 
-    if (((offset / numberOfPolarizations) % 2) != 0){
+    if ((offset % 4) != 0){
+        element.x = -element.x;
+        element.y = -element.y;
+    }
+
+    return element;
+}
+
+__managed__ __device__ cufftCallbackLoadC d_2POL_loadCallbackPtr = CB_ConvertInputC_2POL; 
+
+__device__ cufftComplex CB_ConvertInputC_1POL(void *dataIn, size_t offset, void *callerInfo, void *sharedPtr) {
+    auto element = static_cast<cufftComplex*>(dataIn)[offset];
+
+    if ((offset % 2) != 0){
+        element.x = -element.x;
+        element.y = -element.y;
+    }
+
+    return element;
+}
+
+__managed__ __device__ cufftCallbackLoadC d_1POL_loadCallbackPtr = CB_ConvertInputC_1POL; 
+
+__device__ cufftComplex CB_ConvertInputC(void *dataIn, size_t offset, void *callerInfo, void *sharedPtr) {
+    const auto& inverseStride = static_cast<U64*>(callerInfo)[0];
+    auto element = static_cast<cufftComplex*>(dataIn)[offset];
+
+    if ((offset % inverseStride) != 0){
         element.x = -element.x;
         element.y = -element.y;
     }
@@ -18,15 +44,27 @@ __device__ cufftComplex CB_ConvertInputC(void *dataIn, size_t offset, void *call
 __managed__ __device__ cufftCallbackLoadC d_loadCallbackPtr = CB_ConvertInputC; 
 
 Callback::Callback(cufftHandle& plan, const U64& numberOfPolarizations) {
-    BL_CUDA_CHECK_THROW(cudaMallocManaged(&callerInfo, sizeof(U64)), [&]{
-        BL_FATAL("Can't allocate CUDA memory: {}", err);
-    });
-    this->callerInfo[0] = static_cast<U64>(numberOfPolarizations);
+    void** callerInfoPtr = nullptr;
+    void** callbackPointer = nullptr;
+
+    if (numberOfPolarizations == 1) {
+        callbackPointer = (void**)&d_1POL_loadCallbackPtr;
+    } else if (numberOfPolarizations == 2) {
+        callbackPointer = (void**)&d_2POL_loadCallbackPtr;
+    } else {
+        BL_CUDA_CHECK_THROW(cudaMallocManaged(&callerInfo, sizeof(U64)), [&]{
+            BL_FATAL("Can't allocate CUDA memory: {}", err);
+        });
+        this->callerInfo[0] = static_cast<U64>(numberOfPolarizations) * 2;
+
+        callerInfoPtr = (void**)&callerInfo;
+        callbackPointer = (void**)&d_loadCallbackPtr;
+    }
 
     BL_CUFFT_CHECK_THROW(cufftXtSetCallback(plan, 
-                                            (void**)&d_loadCallbackPtr, 
+                                            callbackPointer, 
                                             CUFFT_CB_LD_COMPLEX,
-                                            (void**)&callerInfo), [&]{
+                                            callerInfoPtr), [&]{
         BL_FATAL("The configuration of an cuFFT callback failed: {0:#x}", err);
     });
 
