f7ef5125bc4b28b03cfd659982adee4a7dea28a2
Luigi F. Cruz <luigifcruz@gmail.com>
2023-05-18 21:27:07 -0300

    Extend benchmark tests for half-precision polarizer.

--- benchmarks/modules/polarizer/generic.cc
+++ benchmarks/modules/polarizer/generic.cc
@@ -3,37 +3,76 @@
 using namespace Blade;
 namespace bm = benchmark;
 
-// TODO: Add more data types.
-static void BM_Polarizer_Compute(bm::State& state) {
+// CF32 -> CF32
+
+static void BM_Polarizer_Compute_CF32_CF32(bm::State& state) {
     ModuleUnderTest<Modules::Polarizer, CF32, CF32> mud;
     BL_CHECK_THROW(mud.runComputeBenchmark(state));
 }
 
-BENCHMARK(BM_Polarizer_Compute)
+BENCHMARK(BM_Polarizer_Compute_CF32_CF32)
     ->Iterations(2<<13)
     ->Args({ 2, 1})
     ->Args({16, 1})
     ->UseManualTime()
     ->Unit(bm::kMillisecond);
 
-static void BM_Polarizer_Transfer(bm::State& state) {
+static void BM_Polarizer_Transfer_CF32_CF32(bm::State& state) {
     ModuleUnderTest<Modules::Polarizer, CF32, CF32> mud;
     BL_CHECK_THROW(mud.runTransferBenchmark(state));
 }
 
-BENCHMARK(BM_Polarizer_Transfer)
+BENCHMARK(BM_Polarizer_Transfer_CF32_CF32)
     ->Iterations(64)
     ->Args({ 2, 1})
     ->Args({16, 1})
     ->UseManualTime()
     ->Unit(bm::kMillisecond);
 
-static void BM_Polarizer_Converged(bm::State& state) {
+static void BM_Polarizer_Converged_CF32_CF32(bm::State& state) {
     ModuleUnderTest<Modules::Polarizer, CF32, CF32> mud;
     BL_CHECK_THROW(mud.runConvergedBenchmark(state));
 }
 
-BENCHMARK(BM_Polarizer_Converged)
+BENCHMARK(BM_Polarizer_Converged_CF32_CF32)
+    ->Iterations(64)
+    ->Args({ 2, 1})
+    ->Args({16, 1})
+    ->UseManualTime()
+    ->Unit(bm::kMillisecond);
+
+// CF16 -> CF16
+
+static void BM_Polarizer_Compute_CF16_CF16(bm::State& state) {
+    ModuleUnderTest<Modules::Polarizer, CF16, CF16> mud;
+    BL_CHECK_THROW(mud.runComputeBenchmark(state));
+}
+
+BENCHMARK(BM_Polarizer_Compute_CF16_CF16)
+    ->Iterations(2<<13)
+    ->Args({ 2, 1})
+    ->Args({16, 1})
+    ->UseManualTime()
+    ->Unit(bm::kMillisecond);
+
+static void BM_Polarizer_Transfer_CF16_CF16(bm::State& state) {
+    ModuleUnderTest<Modules::Polarizer, CF16, CF16> mud;
+    BL_CHECK_THROW(mud.runTransferBenchmark(state));
+}
+
+BENCHMARK(BM_Polarizer_Transfer_CF16_CF16)
+    ->Iterations(64)
+    ->Args({ 2, 1})
+    ->Args({16, 1})
+    ->UseManualTime()
+    ->Unit(bm::kMillisecond);
+
+static void BM_Polarizer_Converged_CF16_CF16(bm::State& state) {
+    ModuleUnderTest<Modules::Polarizer, CF16, CF16> mud;
+    BL_CHECK_THROW(mud.runConvergedBenchmark(state));
+}
+
+BENCHMARK(BM_Polarizer_Converged_CF16_CF16)
     ->Iterations(64)
     ->Args({ 2, 1})
     ->Args({16, 1})
