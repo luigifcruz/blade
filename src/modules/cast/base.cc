790d5f78e912be42c708caaa84dc9ebe1b94310b
Luigi F. Cruz <luigifcruz@gmail.com>
2023-07-10 20:25:46 -0300

    Add bypass capabilities to Cast module.

--- src/modules/cast/base.cc
+++ src/modules/cast/base.cc
@@ -36,15 +36,19 @@ Cast<IT, OT>::Cast(const Config& config,
         )
     );
 
-    // TODO: Add same type automatic bypass.
-
-    // Allocate output buffers.
-    output.buf = ArrayTensor<Device::CUDA, OT>(getOutputBufferShape());
-
     // Print configuration values.
     BL_INFO("Type: {} -> {}", TypeInfo<IT>::name, TypeInfo<OT>::name);
     BL_INFO("Shape: {} -> {}", getInputBuffer().shape(), 
                                getOutputBuffer().shape());
+
+    if constexpr (std::is_same<IT, OT>::value) {
+        BL_DEBUG("Bypassing cast because input and output types are the same.");
+        BL_CHECK_THROW(Memory::Link(output.buf, input.buf));
+        return;
+    }
+
+    // Allocate output buffers.
+    output.buf = ArrayTensor<Device::CUDA, OT>(getOutputBufferShape());
 }
 
 template<typename IT, typename OT>
@@ -52,25 +56,26 @@ Result Cast<IT, OT>::process(const cudaStream_t& stream, const U64& currentCompu
     return this->runKernel("main", stream, input.buf.data(), output.buf.data());
 }
 
+template class BLADE_API Cast<CI8, CI8>;
+template class BLADE_API Cast<CF16, CF16>;
+template class BLADE_API Cast<CF32, CF32>;
+template class BLADE_API Cast<I8, I8>;
+template class BLADE_API Cast<F16, F16>;
+template class BLADE_API Cast<F32, F32>;
+
 template class BLADE_API Cast<CI8, CF32>;
 template class BLADE_API Cast<CI8, CF16>;
-
 template class BLADE_API Cast<CF16, F16>;
 template class BLADE_API Cast<CF16, F32>;
 template class BLADE_API Cast<CF16, CF32>;
-
 template class BLADE_API Cast<CF32, F16>;
 template class BLADE_API Cast<CF32, F32>;
 template class BLADE_API Cast<CF32, CF16>;
-
 template class BLADE_API Cast<F16, F32>;
 template class BLADE_API Cast<F16, CF32>;
 template class BLADE_API Cast<F16, CF16>;
-
 template class BLADE_API Cast<F32, F16>;
 template class BLADE_API Cast<F32, CF32>;
 template class BLADE_API Cast<F32, CF16>;
-
-template class BLADE_API Cast<CF32, CF32>;
 
 }  // namespace Blade::Modules
