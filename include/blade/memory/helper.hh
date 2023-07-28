259c01cedacdf8fddcc1ce096106f9c5ed005cef
Luigi F. Cruz <luigifcruz@gmail.com>
2023-07-27 22:31:42 -0300

    Add Memory::Duet helper.

--- include/blade/memory/helper.hh
+++ include/blade/memory/helper.hh
@@ -1,12 +1,42 @@
 #ifndef BLADE_MEMORY_HELPER_HH
 #define BLADE_MEMORY_HELPER_HH
 
+#include <vector>
+
 #include <cuda_runtime.h>
 
 #include "blade/memory/types.hh"
 #include "blade/memory/vector.hh"
 
-namespace Blade::Memory {
+namespace Blade {
+
+template<typename T>
+class BLADE_API Duet {
+ public:
+    Duet(const U64& number)
+        : swapchain(2, T({number})) {
+        _fixed = swapchain[0];
+    }
+
+    template<typename... Args>
+    Duet(Args&&... args)
+        : swapchain(2, T(std::forward<Args>(args)...)) {
+        _fixed = swapchain[0];
+    }
+
+    operator T&() {
+        return _fixed;
+    }
+
+    T& operator[](const U64& index) {
+        _fixed = swapchain[index];
+        return _fixed;
+    }
+
+ private:
+    T _fixed;
+    std::vector<T> swapchain;
+};
 
 template<typename Type, typename Dims>
 inline Result PageLock(const Vector<Device::CPU, Type, Dims>& vec,
@@ -69,6 +99,6 @@ inline std::string ReadableBytes(uint64_t bytes) {
     return std::string(buffer);
 }
 
-}  // namespace Blade::Memory
+}  // namespace Blade
 
 #endif
