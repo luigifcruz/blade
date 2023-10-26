07d8afec581aa3725dd6e9584c7a9d1c4b885599
Luigi F. Cruz <luigifcruz@gmail.com>
2023-10-26 16:26:18 -0300

    Port beamformer test to the new Python API.

--- python/blade/ext_beamformer.cc
+++ python/blade/ext_beamformer.cc
@@ -7,13 +7,40 @@ namespace nb = nanobind;
 using namespace nb::literals;
 using namespace Blade;
 
-template<template<typename...> class BaseClass, typename IT, typename OT>
-void NB_SUBMODULE(auto& m, const auto& name) {
-    using Class = BaseClass<IT, OT>;
+template<template<typename...> class Base, 
+         template<typename...> class Generic, 
+         typename IT, 
+         typename OT>
+void NB_SUBMODULE_TELESCOPE(auto& m, const auto& tel_name, const auto& in_name, const auto& out_name) {
+    using Class = Base<IT, OT>;
+    using GenericClass = Generic<IT, OT>;
 
-    nb::class_<Class, Module> mod(m, name);
+    auto mm = m.def_submodule(tel_name)
+               .def_submodule(in_name)
+               .def_submodule(out_name);
 
-    nb::class_<typename Class::Config>(mod, "config")
+    nb::class_<Class, GenericClass> mod(mm, "mod");
+
+    mod
+        .def(nb::init<const typename GenericClass::Config&,
+                      const typename GenericClass::Input&,
+                      const Stream&>(), "config"_a,
+                                        "input"_a,
+                                        "stream"_a);
+}
+
+template<template<typename...> class Generic, 
+         typename IT, 
+         typename OT>
+void NB_SUBMODULE_GENERIC(auto& m, const auto& in_name, const auto& out_name) {
+    using GenericClass = Generic<IT, OT>;
+
+    auto mm = m.def_submodule(in_name)
+               .def_submodule(out_name);
+
+    nb::class_<GenericClass, Module> mod(mm, "_generic_mod");
+
+    nb::class_<typename GenericClass::Config>(mod, "config")
         .def(nb::init<const BOOL&,
                       const BOOL&,
                       const U64&>(),
@@ -21,42 +48,44 @@ void NB_SUBMODULE(auto& m, const auto& name) {
                                      "enable_incoherent_beam_sqrt"_a,
                                      "block_size"_a = 512);
 
-    nb::class_<typename Class::Input>(mod, "input")
+    nb::class_<typename GenericClass::Input>(mod, "input")
         .def(nb::init<const ArrayTensor<Device::CUDA, IT>&,
                       const PhasorTensor<Device::CUDA, IT>&>(), "buffer"_a,
                                                                 "phasors"_a);
 
     mod
-        .def(nb::init<const typename Class::Config&,
-                      const typename Class::Input&,
-                      const Stream&>(), "config"_a,
-                                        "input"_a,
-                                        "stream"_a)
-        .def("process", [](Class& instance, const U64& counter) {
+        .def("process", [](GenericClass& instance, const U64& counter) {
             return instance.process(counter);
         })
-        .def("get_config", &Class::getConfig, nb::rv_policy::reference)
-        .def("get_input_buffer", &Class::getInputBuffer, nb::rv_policy::reference)
-        .def("get_input_phasors", &Class::getInputPhasors, nb::rv_policy::reference)
-        .def("get_output", &Class::getOutputBuffer, nb::rv_policy::reference)
-        .def("__repr__", [](Class& obj){
+        .def("get_config", &GenericClass::getConfig, nb::rv_policy::reference)
+        .def("get_input_buffer", &GenericClass::getInputBuffer, nb::rv_policy::reference)
+        .def("get_input_phasors", &GenericClass::getInputPhasors, nb::rv_policy::reference)
+        .def("get_output", &GenericClass::getOutputBuffer, nb::rv_policy::reference)
+        .def("__repr__", [](GenericClass& obj){
             return fmt::format("Beamformer()");
         });
 }
 
 NB_MODULE(_beamformer_impl, m) {
+    // Generic classes.
+
+    NB_SUBMODULE_GENERIC<Modules::Beamformer::Generic, CF32, CF32>(m, "in_cf32", "out_cf32");
+
+    // Telescope specific classes.
+
 #ifdef BLADE_MODULE_ATA_BEAMFORMER
-    {
-        auto mm = m.def_submodule("tel_ata");
-        NB_SUBMODULE<Modules::Beamformer::ATA, CF32, CF32>(mm, "type_cf32");
-    }
+    NB_SUBMODULE_TELESCOPE<Modules::Beamformer::ATA, 
+                           Modules::Beamformer::Generic, 
+                           CF32, 
+                           CF32>(m, "tel_ata", "in_cf32", "out_cf32");
 #endif
 #ifdef BLADE_MODULE_MEERKAT_BEAMFORMER
-    {
-        auto mm = m.def_submodule("tel_meerkat");
-        NB_SUBMODULE<Modules::Beamformer::MeerKAT, CF32, CF32>(mm, "type_cf32");
-    }
+    NB_SUBMODULE_TELESCOPE<Modules::Beamformer::MeerKAT, 
+                           Modules::Beamformer::Generic,
+                           CF32,
+                           CF32>(m, "tel_meerkat", "in_cf32", "out_cf32");
 #endif
 #ifdef BLADE_MODULE_VLA_BEAMFORMER
+    // TODO: Add VLA beamformer.
 #endif
 }
--- tests/modules/beamformer/ata.py
+++ tests/modules/beamformer/ata.py
@@ -1,77 +1,79 @@
-import time
-import blade as bl
 import numpy as np
+import blade as bl
 
-class Test(bl.Pipeline):
-    beamformer: bl.Beamformer
+@bl.runner
+class Pipeline:
+    def __init__(self, input_shape, phasor_shape, output_shape, config):
+        self.input.buf = bl.array_tensor(input_shape, dtype=bl.cf32)
+        self.input.phasors = bl.phasor_tensor(phasor_shape, dtype=bl.cf32)
+        self.output.buf = bl.array_tensor(output_shape, dtype=bl.cf32)
 
-    def __init__(self, inputDims, phasorDims):
-        bl.Pipeline.__init__(self)
-        self.input = bl.cuda.cf32.ArrayTensor(inputDims)
-        self.phasors = bl.cuda.cf32.PhasorTensor(phasorDims)
-        _config = bl.Beamformer.Config(True, True, 512)
-        _input = bl.Beamformer.Input(self.input, self.phasors)
-        self.beamformer = self.connect(_config, _input)
+        input = (self.input.buf, self.input.phasors)
+        self.module.beamformer = bl.module(bl.beamformer, config, input, telescope=bl.ata)
 
-    def run(self, input: bl.cpu.cf32.ArrayTensor,
-                  phasors: bl.cpu.cf32.PhasorTensor,
-                  output: bl.cpu.cf32.ArrayTensor):
-        self.copy(self.beamformer.input(), input)
-        self.copy(self.beamformer.phasors(), phasors)
-        self.compute()
-        self.copy(output, self.beamformer.output())
-        self.synchronize()
+    def transfer_in(self, buf, phasors):
+        self.copy(self.input.buf, buf)
+        self.copy(self.input.phasors, phasors)
+
+    def transfer_out(self, buf):
+        self.copy(self.output.buf, self.module.beamformer.get_output())
+        self.copy(buf, self.output.buf)
 
 
 if __name__ == "__main__":
     # Specify dimension of array.
-    inputDims = (2, 192, 512, 2)
-    phasorDims = (1, 2, 192, 1, 2)
-    outputDims = (2, 192, 512, 2)
+    input_shape = (2, 192, 512, 2)
+    phasor_shape = (1, 2, 192, 1, 2)
+    output_shape = (2, 192, 512, 2)
 
-    # Initialize Blade pipeline.
-    mod = Test(inputDims, phasorDims)
+    config = {
+        'enable_incoherent_beam': True,
+        'enable_incoherent_beam_sqrt': True,
+    }
 
     # Generate test data with Python.
-    _a = np.random.uniform(-int(2**8/2), int(2**8/2), inputDims)
-    _b = np.random.uniform(-int(2**8/2), int(2**8/2), inputDims)
+    _a = np.random.uniform(-int(2**8/2), int(2**8/2), input_shape)
+    _b = np.random.uniform(-int(2**8/2), int(2**8/2), input_shape)
     input = np.array(_a + _b * 1j).astype(np.complex64)
 
-    _a = np.zeros(phasorDims, dtype=np.complex64)
+    _a = np.zeros(phasor_shape, dtype=np.complex64)
     phasors = np.random.random(size=_a.shape) + 1j*np.random.random(size=_a.shape)
 
-    output = np.zeros(outputDims, dtype=np.complex64)
+    output = np.zeros(output_shape, dtype=np.complex64)
 
     # Import test data from Python to Blade.
-    bl_input = bl.cpu.cf32.ArrayTensor(inputDims)
-    bl_phasors = bl.cpu.cf32.PhasorTensor(phasorDims)
-    bl_output = bl.cpu.cf32.ArrayTensor(outputDims)
+    host_input = bl.array_tensor(input_shape, dtype=bl.cf32, device=bl.cpu)
+    host_phasors = bl.phasor_tensor(phasor_shape, dtype=bl.cf32, device=bl.cpu)
+    host_output = bl.array_tensor(output_shape, dtype=bl.cf32, device=bl.cpu)
 
-    np.copyto(bl_input.asnumpy(), input)
-    np.copyto(bl_phasors.asnumpy(), phasors)
-    np.copyto(bl_output.asnumpy(), output)
+    np.copyto(host_input.as_numpy(), input)
+    np.copyto(host_phasors.as_numpy(), phasors)
+    np.copyto(host_output.as_numpy(), output)
 
-    # Beamform with Blade.
-    start = time.time()
-    mod.run(bl_input, bl_phasors, bl_output)
-    print(f"Beamform with Blade took {time.time()-start:.2f} s.")
+    #
+    # Blade Implementation
+    #
 
-    # Beamform with Numpy.
-    start = time.time()
+    pipeline = Pipeline(input_shape, phasor_shape, output_shape, config)
+    pipeline(host_input, host_phasors, host_output)
+
+    #
+    # Python Implementation
+    #
+
     for ibeam in range(phasors.shape[0]):
         phased = input * phasors[ibeam][..., :]
         output[ibeam] = phased.sum(axis=0)
     phased = input * phasors[-1][..., :]
     phased = (phased.real * phased.real) + (phased.imag * phased.imag)
     output[-1] = np.sqrt(phased.sum(axis=0))
-    print(f"Beamform with Numpy took {time.time()-start:.2f} s.")
 
-    # Check both answers.
-    bl_out = bl_output.asnumpy()
+    #
+    # Compare Results
+    #
+
+    bl_out = host_output.as_numpy()
     py_out = output
-
-    print(bl_out[0, 0, :16, 0])
-    print(py_out[0, 0, :16, 0])
 
     assert np.allclose(bl_out[:-1, :, :, :], py_out[:-1, :, :, :], rtol=0.01)
     assert np.allclose(bl_out[-1, :, :, :], py_out[-1, :, :, :], atol=250)
