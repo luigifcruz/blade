79876c1e6a4d02a946a80e3ad73cff402a570191
Luigi F. Cruz <luigifcruz@gmail.com>
2023-10-30 16:34:09 -0300

    Port channelizer test to the new Python API.

--- python/blade/ext_channelizer.cc
+++ python/blade/ext_channelizer.cc
@@ -8,10 +8,13 @@ using namespace nb::literals;
 using namespace Blade;
 
 template<typename IT, typename OT>
-void NB_SUBMODULE(auto& m, const auto& name) {
+void NB_SUBMODULE(auto& m, const auto& in_name, const auto& out_name) {
     using Class = Modules::Channelizer<IT, OT>;
 
-    nb::class_<Class, Module> mod(m, name);
+    auto mm = m.def_submodule(in_name)
+               .def_submodule(out_name);
+
+    nb::class_<Class, Module> mod(mm, "mod");
 
     nb::class_<typename Class::Config>(mod, "config")
         .def(nb::init<const U64&,
@@ -39,5 +42,5 @@ void NB_SUBMODULE(auto& m, const auto& name) {
 }
 
 NB_MODULE(_channelizer_impl, m) {
-    NB_SUBMODULE<CF32, CF32>(m, "type_cf32");
+    NB_SUBMODULE<CF32, CF32>(m, "in_cf32", "out_cf32");
 }
