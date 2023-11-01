dd056c982e2c0b4e4845b49fc66eac7d53a3610b
Luigi F. Cruz <luigifcruz@gmail.com>
2023-11-01 18:22:32 -0300

    Update matrix test.

--- tests/memory/vector/matrix.py
+++ tests/memory/vector/matrix.py
@@ -5,17 +5,17 @@ from numpy import random
 
 # Human Version
 
-vec_bl = bl.cpu.f32.ArrayTensor((1, 1, 10, 2))
-vec_np = vec_bl.asnumpy()
+vec_bl = bl.array_tensor((1, 1, 10, 2), dtype=bl.f32, device=bl.cpu)
+vec_np = vec_bl.as_numpy()
 
 for i in range(10):
     vec_np[0, 0, i, 0] = 0.69
     vec_np[0, 0, i, 1] = 0.42
 
 print(vec_np)
-print(vec_bl.asnumpy())
+print(vec_bl.as_numpy())
 
-assert np.allclose(vec_np, vec_bl.asnumpy(), rtol=0.1)
+assert np.allclose(vec_np, vec_bl.as_numpy(), rtol=0.1)
 
 for i in range(10):
     print(vec_bl[0, 0, i, 0], vec_bl[0, 0, i, 1])
@@ -25,12 +25,12 @@ for i in range(10):
 
 # Super Human Version
 
-vec_bl = bl.cpu.f32.ArrayTensor((3, 13, 9, 11))
-vec_np = vec_bl.asnumpy()
+vec_bl = bl.array_tensor((3, 13, 9, 11), dtype=bl.f32, device=bl.cpu)
+vec_np = vec_bl.as_numpy()
 
 vec_np[:, :, :, :] = random.rand(*vec_np.shape)
 
-assert np.allclose(vec_np, vec_bl.asnumpy(), rtol=0.1)
+assert np.allclose(vec_np, vec_bl.as_numpy(), rtol=0.1)
 
 for a in range(vec_np.shape[0]):
     for b in range(vec_np.shape[1]):
