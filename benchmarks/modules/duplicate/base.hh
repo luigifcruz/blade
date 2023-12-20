edb9684418b5514160fe9bb99d63820cdd416227
luigifcruz <github@luigi.ltd>
2023-12-20 15:58:21 -0300

    Add Duplicate Module benchmarks.

--- benchmarks/modules/duplicate/base.hh
+++ benchmarks/modules/duplicate/base.hh
@@ -7,7 +7,33 @@
 
 namespace Blade {
 
-// TODO: Add Duplicate benchmark.
+template<template<typename, typename> class MUT, typename IT, typename OT>
+class DuplicateTest : CudaBenchmark {
+ public:
+    typename MUT<IT, OT>::Config config;
+    std::shared_ptr<MUT<IT, OT>> module;
+    ArrayTensor<Device::CUDA, IT> deviceInputBuf;
+
+    Result run(benchmark::State& state) {
+        InitAndProfile([&](){
+            deviceInputBuf = ArrayTensor<Device::CUDA, IT>({20, 192, 8192, 2});
+
+            BL_DISABLE_PRINT();
+            Create(module, config, {
+                .buf = deviceInputBuf, 
+            }, this->getStream());
+            BL_ENABLE_PRINT();
+        }, state);
+
+        for (auto _ : state) {
+            BL_CHECK(this->startIteration());
+            BL_CHECK(module->process(0, this->getStream()));
+            BL_CHECK(this->finishIteration(state));
+        }
+
+        return Result::SUCCESS;
+    }
+};
 
 }  // namespace Blade
 
--- benchmarks/modules/duplicate/generic.hh
+++ benchmarks/modules/duplicate/generic.hh
@@ -3,3 +3,26 @@
 using namespace Blade;
 namespace bm = benchmark;
 
+// CF32 -> CF32
+
+static void BM_Duplicate_Compute_CF32_CF32(bm::State& state) {
+    DuplicateTest<Modules::Duplicate, CF32, CF32> mud;
+    BL_CHECK_THROW(mud.run(state));
+}
+
+BENCHMARK(BM_Duplicate_Compute_CF32_CF32)
+    ->Iterations(2<<13)
+    ->UseManualTime()
+    ->Unit(bm::kMillisecond);
+
+// F32 -> F32
+
+static void BM_Duplicate_Compute_F32_F32(bm::State& state) {
+    DuplicateTest<Modules::Duplicate, F32, F32> mud;
+    BL_CHECK_THROW(mud.run(state));
+}
+
+BENCHMARK(BM_Duplicate_Compute_F32_F32)
+    ->Iterations(2<<13)
+    ->UseManualTime()
+    ->Unit(bm::kMillisecond);
