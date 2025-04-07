9be7c3cbd9a1ace9ae93606a9c0aa44e589f913c
Luigi Cruz <github@luigi.ltd>
2025-04-07 13:23:47 -0700

    Add `CALC_MODE` enum.

--- include/blade/types.hh
+++ include/blade/types.hh
@@ -23,7 +23,7 @@ struct UVW {
     double W;
 };
 
-struct LLA { 
+struct LLA {
     double LON;
     double LAT;
     double ALT;
@@ -48,6 +48,42 @@ enum class POL : uint8_t {
     LR,
 };
 
+#ifndef __CUDA_ARCH__
+inline std::ostream& operator<<(std::ostream& os, const POL& pol) {
+    switch (pol) {
+        case POL::X: return os << "X";
+        case POL::Y: return os << "Y";
+        case POL::L: return os << "L";
+        case POL::R: return os << "R";
+        case POL::XY: return os << "XY";
+        case POL::LR: return os << "LR";
+    }
+    return os;
+}
+#endif
+
+enum class CALC_MODE : uint8_t {
+    INTEGER,
+    SINGLE_PRECISION_FP,
+    DOUBLE_PRECISION_FP
+};
+
+#ifndef __CUDA_ARCH__
+inline std::ostream& operator<<(std::ostream& os, const CALC_MODE& mode) {
+    switch (mode) {
+        case CALC_MODE::INTEGER: return os << "Integer";
+        case CALC_MODE::SINGLE_PRECISION_FP: return os << "Single Precision Floating Point";
+        case CALC_MODE::DOUBLE_PRECISION_FP: return os << "Double Precision Floating Point";
+    }
+    return os;
+}
+#endif
+
 }  // namespace Blade
+
+#ifndef __CUDA_ARCH__
+template <> struct bl::fmt::formatter<Blade::POL> : ostream_formatter {};
+template <> struct bl::fmt::formatter<Blade::CALC_MODE> : ostream_formatter {};
+#endif
 
 #endif
