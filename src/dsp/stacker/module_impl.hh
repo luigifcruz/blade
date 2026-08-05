729a58e4654ed495de227e6081d5ef049527d588
Luigi Cruz <github@luigi.ltd>
2026-08-05 05:58:23 +0000

    refactor(stacker): validate stacking plans up front

--- src/dsp/stacker/module_impl.hh
+++ src/dsp/stacker/module_impl.hh
@@ -3,6 +3,7 @@
 
 #include <blade/stacker/module.hh>
 #include <jetstream/detail/module_impl.hh>
+#include <jetstream/memory/axis.hh>
 
 namespace Jetstream::Modules {
 
@@ -14,9 +15,30 @@ struct StackerImpl : public Module::Impl, public DynamicConfig<Stacker> {
     Result reconfigure() override;
 
  protected:
+    Tensor validatedInputTensor;
+    Shape validatedOutputShape;
+    SignalAxes validatedSignalAxes;
+    bool validatedBypass = false;
+    U64 validatedRatio = 1;
+    U64 validatedWidth = 0;
+    U64 validatedOutputWidth = 0;
+    U64 validatedWidthByteSize = 0;
+    U64 validatedOutputRowByteSize = 0;
+    U64 validatedHeight = 0;
+    U64 validatedInputSize = 0;
+    U64 validatedOutputSizeBytes = 0;
+
     Tensor inputTensor;
     Tensor outputTensor;
     bool bypass = false;
+    U64 stackRatio = 1;
+    U64 width = 0;
+    U64 outputWidth = 0;
+    U64 widthByteSize = 0;
+    U64 outputRowByteSize = 0;
+    U64 height = 0;
+    U64 inputSize = 0;
+    U64 stackIndex;
 };
 
 }  // namespace Jetstream::Modules
